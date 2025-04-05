bd0ae0b9eae18174897df37fe490329cedd6b718
shang8024 <shang8024@gmail.com>
2025-04-05 15:07:32 -0400

    refactor: refactor project structure, add eslint, shadcn-ui

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["*.world-is-a-library.com","world-is-a-library.com","*.localhost"],
 };
 
 export default nextConfig;
