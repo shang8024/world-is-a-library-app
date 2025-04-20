680bdb084a77157d6b476050935de7ab1cc167be
shang8024 <shang8024@gmail.com>
2025-04-20 04:43:57 -0400

    core: redend opt out

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -17,29 +17,29 @@ export const auth = betterAuth({
     autoSignIn: false,
     minPasswordLength: 8,
     maxPasswordLength: 20,
-    requireEmailVerification: true, //It does not allow user to login without email verification [!code highlight]
-    sendResetPassword: async ({ user, url }) => {
-      await resend.emails.send({
+    requireEmailVerification: process.env.RESEND_API_KEY !== undefined, // Require email verification for sign up
+    sendResetPassword: process.env.RESEND_API_KEY ? async ({ user, url }) => {
+      await resend?.emails.send({
         from: sender_email, 
         to: user.email, // email of the user to want to end
         subject: "Password reset", // Main subject of the email
         html: `Click the link to reset your password: ${url}`, // Content of the email
             //TODO: add a react template for the email
       });
-    },
+    }: undefined, // Send reset password email
   },
   emailVerification: {
-    sendOnSignUp: true, // Automatically sends a verification email at signup
+    sendOnSignUp: process.env.RESEND_API_KEY !== undefined, // Send verification email on sign up
     autoSignInAfterVerification: true, // Automatically signIn the user after verification
-    sendVerificationEmail: async ({ user, url }) => {
-      await resend.emails.send({
+    sendVerificationEmail: process.env.RESEND_API_KEY ? async ({ user, url }) => {
+      await resend?.emails.send({
         from: sender_email, 
         to: user.email, // email of the user to want to end
         subject: "Email Verification", // Main subject of the email
         html: `Click the link to verify your email: ${url}`, // Content of the email
         //TODO: add a react template for the email
       });
-    },
+    }: undefined, // Send verification email
   },
   session: {
     expiresIn: 60 * 60 * 24 * 7, // 7 days
